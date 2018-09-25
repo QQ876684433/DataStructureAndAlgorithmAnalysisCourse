f2c06875b4553b1e40c08c974f33de8b27d18545
steve <chph13420146901@gmail.com>
2018-09-25 20:30:00 +0800

    添加简单注释

--- Chapter1/1.cpp
+++ Chapter1/1.cpp
@@ -36,15 +36,23 @@ int main() {
     return 0;
 }
 
+/**
+ *
+ * @param num 所有数据,当找到一个组合时，0～r-1的数就是结果
+ * @param p 计数，当p==r时说明已经找到一个组合
+ * @param r 任取的r个数
+ * @param n 数据的个数
+ * @param cur 指针
+ */
 void Perm(int *num, int p, int r, int n, int cur) {
-    if (p == r) {
+    if (p == r) {//找到一个组合
         for (int i = 0; i < r; ++i) {
             cout << num[i];
         }
         cout << endl;
     }
     for (int i = cur; i < n; ++i) {
-        if (i + r - p > n) break;
+        if (i + r - p > n) break;//判断是否越界
         swap(num[p], num[i]);
         Perm(num, p + 1, r, n, i + 1);
         swap(num[p], num[i]);
--- Chapter1/2.cpp
+++ Chapter1/2.cpp
@@ -19,6 +19,14 @@ int main() {
     return 0;
 }
 
+/**
+ * 如果n==1说明可以直接把它移动到目标位置
+ *
+ * @param n 要移动的盘子数
+ * @param from 盘子所在的位置
+ * @param to 盘子要移动的目标位置
+ * @param temp 把n-1个盘子暂时移到的位置
+ */
 void solve(int n, char from, char to, char temp) {
     times++;
     if (n == 1) cout << from << " to " << to << endl;
