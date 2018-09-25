6c2c411775a04bdfe0e39c4dcea213a435a7b38a
steve <chph13420146901@gmail.com>
2018-09-25 11:06:49 +0800

    中缀表达式 -> 后缀表达式 -> 对后缀表达式求值, 合为一趟来做。

---
+++ Chapter_3_1/5.cpp
@@ -0,0 +1,82 @@
+//
+// Created by steve on 18-9-25.
+//
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int getResult(string exp);
+
+void calculate(char ch, stack<int> &operand);
+
+int main() {
+    string exp;
+    getline(cin, exp);
+    cout << getResult(exp) << endl;
+    return 0;
+}
+
+int getResult(string exp) {
+    stack<int> operand;//操作数栈
+    stack<char> operation;//操作符栈
+    for (int i = 0; i < exp.length(); ++i) {
+        char ch = exp[i];
+        if (isdigit(ch)) {
+            int num = 0;
+            int j;
+            for (j = i; j < exp.length(); ++j) {
+                if (!isdigit(exp[j])) break;
+                num = num * 10 + exp[j] - '0';
+            }
+            i = j - 1;
+            operand.push(num);//操作数入栈
+        } else if (ch == '(') operation.push(ch);
+        else if (ch == ')') {
+            while (operation.top() != '(') {
+                calculate(operation.top(), operand);
+                operation.pop();
+            }
+        } else if (ch == '+' || ch == '-') {
+            while (!operation.empty() && (operation.top() == '+' || operation.top() == '-' || operation.top() == '*' ||
+                                          operation.top() == '/')) {
+                calculate(operation.top(), operand);
+                operation.pop();
+            }
+            operation.push(ch);
+        } else if (ch == '*' || ch == '/') {
+            while (!operation.empty() && (operation.top() == '*' || operation.top() == '/')) {
+                calculate(operation.top(), operand);
+                operation.pop();
+            }
+            operation.push(ch);
+        }
+    }
+    while (!operation.empty()) {
+        calculate(operation.top(), operand);
+        operation.pop();
+    }
+    return operand.top();
+}
+
+void calculate(char ch, stack<int> &operand) {
+    int a = operand.top();
+    operand.pop();
+    switch (ch) {
+        case '+':
+            operand.top() += a;
+            break;
+        case '-':
+            operand.top() -= a;
+            break;
+        case '*':
+            operand.top() *= a;
+            break;
+        case '/':
+            operand.top() /= a;
+            break;
+        default:
+            break;
+    }
+}
