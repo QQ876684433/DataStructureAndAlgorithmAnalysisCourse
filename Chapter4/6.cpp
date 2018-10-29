1d791b2e7217bce93f03d5bf5064a6ad8f2e3283
steve <chph13420146901@gmail.com>
2018-10-29 22:49:36 +0800

    fx作业备份

--- Chapter4/6.cpp
+++ Chapter4/6.cpp
@@ -8,8 +8,10 @@
 
 #include <iostream>
 #include <stack>
+#include <queue>
 
 using namespace std;
+
 
 struct BinTreeNode {
     char data;
@@ -17,6 +19,15 @@ struct BinTreeNode {
 
     BinTreeNode(char x, BinTreeNode *l = NULL, BinTreeNode *r = NULL) : data(x), leftChild(l),
                                                                         rightChild(r) {}
+};
+
+struct stkNode {
+    BinTreeNode *ptr;
+    enum {
+        L, R
+    } tag;
+
+    stkNode(BinTreeNode *p) : ptr(p), tag(L) {}
 };
 
 void PreOrder(BinTreeNode *subTree) {
@@ -27,12 +38,47 @@ void PreOrder(BinTreeNode *subTree) {
     }
 }
 
+void PreOrderNotRecursive(BinTreeNode *subTree) {
+    stack<BinTreeNode *> s;
+    BinTreeNode *p = subTree;
+    s.push(NULL);
+    while (p) {
+        cout << p->data;
+        if (p->rightChild)s.push(p->rightChild);
+        if (p->leftChild) p = p->leftChild;
+        else {
+            p = s.top();
+            s.pop();
+        }
+    }
+    cout << endl;
+}
+
 void InOrder(BinTreeNode *subTree) {
     if (subTree != NULL) {
         InOrder(subTree->leftChild);
         cout << subTree->data;
         InOrder(subTree->rightChild);
     }
+}
+
+void InOrderNotRecursive(BinTreeNode *subTree) {
+    BinTreeNode *cur = subTree;
+    stack<BinTreeNode *> s;
+    s.push(NULL);
+    do {
+        while (cur) {
+            s.push(cur);
+            cur = cur->leftChild;
+        }
+        cur = s.top();
+        s.pop();
+        if (!s.empty()) {
+            cout << cur->data;
+            cur = cur->rightChild;
+        }
+    } while (cur || !s.empty());
+    cout << endl;
 }
 
 void PostOrder(BinTreeNode *subTree) {
@@ -43,23 +89,76 @@ void PostOrder(BinTreeNode *subTree) {
     }
 }
 
-BinTreeNode *makeTree(BinTreeNode *&root, char *GeneralList) {
-    //通过广义表来构建二叉树
-    char ch = GeneralList[0];
-    GeneralList++;
+void PostOrderNotRecursive(BinTreeNode *subTree) {
+    BinTreeNode *p = subTree;
+    stack<stkNode> s;
+    do {
+        while (p) {
+            stkNode w(p);
+            w.tag = w.L;
+            s.push(w);
+            p = p->leftChild;
+        }
+        int continue1 = 1;
+        while (continue1 && !s.empty()) {
+            stkNode w = s.top();
+            p = w.ptr;
+            s.pop();
+            switch (w.tag) {
+                case w.L:
+                    w.tag = w.R;
+                    p = p->rightChild;
+                    continue1 = 0;
+                    s.push(w);
+                    break;
+                case w.R:
+                    cout << p->data;
+                    break;
+            }
+        }
+    } while (!s.empty());
+    cout << endl;
+}
 
-    return root;
+//构造二叉树
+void makeTree(string GeneralList, BinTreeNode *&root) {
+    root = new BinTreeNode(GeneralList[0]);
+    queue<BinTreeNode *> q;
+    q.push(root);
+    for (int i = 1; i < GeneralList.length(); ++i) {
+        BinTreeNode *node = q.front();
+        q.pop();
+        char ch = GeneralList[i];
+        if (ch != '*') {
+            node->leftChild = new BinTreeNode(ch);
+            q.push(node->leftChild);
+        }
+        ch = GeneralList[++i];
+        if (ch != '*') {
+            node->rightChild = new BinTreeNode(ch);
+            q.push(node->rightChild);
+        }
+    }
 }
 
 int main() {
     string genList;
     cin >> genList;
     BinTreeNode *root;
-    makeTree(root, &(genList[0]));
+    makeTree(genList, root);
+    cout << "PreOrder" << endl;
     PreOrder(root);
     cout << endl;
+    PreOrderNotRecursive(root);
+    cout << endl;
+    cout << "InOrder" << endl;
     InOrder(root);
     cout << endl;
+    InOrderNotRecursive(root);
+    cout << endl;
+    cout << "PostOrder" << endl;
     PostOrder(root);
+    cout << endl;
+    PostOrderNotRecursive(root);
     return 0;
 }
